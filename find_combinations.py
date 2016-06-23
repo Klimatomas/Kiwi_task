8276d1d4b552a78deacf25cebbcc671265574e24
Tomas <mrklimatomas@gmail.com>
2016-06-23 18:51:50 +0200

    General update and cleanup

--- find_combinations.py
+++ find_combinations.py
@@ -7,45 +7,46 @@ class Combinatorix(object):
     def __init__(self, source, destination, departure, arrival, flight_number):
         self.source = source
         self.destination = destination
-        self.departure = datetime.datetime.strptime(departure, "%Y-%m-%dT%H:%M:%S")
-        self.arrival = datetime.datetime.strptime(arrival, "%Y-%m-%dT%H:%M:%S")
+        self.departure = datetime.datetime.strptime(departure, '%Y-%m-%dT%H:%M:%S')
+        self.arrival = datetime.datetime.strptime(arrival, '%Y-%m-%dT%H:%M:%S')
         self.flight_number = flight_number.strip()
 
     def __str__(self):
-        return "%s %s %s %s %s" % (self.source, self.destination, self.departure, self.arrival, self.flight_number)
+        return '%s %s %s %s %s' % (self.source, self.destination, self.departure, self.arrival, self.flight_number)
 
 
-def method(item, arr, na=None):
-    if na is None:
-        na = []
-    for i in arr:
-        delta = i.departure - item.arrival
-        x = datetime.timedelta(hours=4)
-        y = datetime.timedelta(hours=1)
-        if y < delta < x and item.destination == i.source and item.source != i.destination:
-            x = [{"A": item.flight_number + item.destination, "B": i.flight_number + i.destination}]
-            na.append(x)
-            method(i, arr, x)
-        else:
-            pass
-    return na
+def combine(item, object_array, container=None):
+    if container is None:
+        container = []
+    for compare in object_array:
+        delta = compare.departure - item.arrival
+        latest = datetime.timedelta(hours=4)
+        earliest = datetime.timedelta(hours=1)
+        if earliest < delta < latest and item.destination == compare.source and item.source != compare.destination:
+            entry = [{'A': item.flight_number + '' + item.destination,
+                      'B': compare.flight_number + '' + compare.destination}]
+            container.append(entry)
+            combine(compare, object_array, entry)
+
+    return container
 
 
 def main():
-    final_arr = []
-    arr = []
+    final_array = {'Flight_List': []}
+    object_array = []
     data = sys.stdin.readlines()
     data.pop(0)  # remove first line
     for item in data:
-        ary = item.split(",")
+        ary = item.split(',')
         x = Combinatorix(ary[0], ary[1], ary[2], ary[3], ary[4])
-        arr.append(x)
+        object_array.append(x)
 
-    for item in arr:
-        result = method(item, arr)
+    for item in object_array:
+        result = combine(item, object_array)
         if result:
-            final_arr.append(result)
+            final_array['Flight_List'] += result
 
-    print dumps(final_arr)
+    sys.stdout.write(dumps(final_array))
+
 
 main()
